58894fffb934717604a74796c1220966180e0a21
sofiamagirescu <109241794+sofiamagirescu@users.noreply.github.com>
2024-07-16 23:26:23 +0200

    Add files via upload

--- OpenAIService.swift
+++ OpenAIService.swift
@@ -1,8 +1,8 @@
 //
 //  OpenAIService.swift
-//  Amin
+//  InfoEducatie
 //
-//  Created by Calin Gavriliu on 10.06.2024.
+//  Created by Sofia Magirescu on 16.07.2024.
 //
 
 import SwiftUI
@@ -10,95 +10,147 @@ import Combine
 import Alamofire
 
 class OpenAIService: ObservableObject {
-    let baseURL = "https://api.openai.com/v1/chat/completions"
+    
+    private let baseURL = "https://api.openai.com/v1/chat/completions"
+    private let model = "gpt-4o"
+    private let temperature: Float = 0.5
+    private let maxTokens: Int = 2048
+    private let defaultRetryCount = 3
+    
+    private let notWorkingErrorResponseMessage = "Imi pare rau, dar nu sunt in stare sa produc un raspuns. Te rog sa incerci mai tarziu."
+    
     @Published var responseMessage: String = ""
-    @Published var isLoading: Bool = false
     @Published var errorMessage: String? = nil
+    
+    @Published var summaryMessage: String = ""
+    @Published var isSummaryLoading: Bool = false
+    
+    @Published var isLoading: Bool = false
     @Published var isInitialLoading: Bool = true
-    var conversationHistory: [[String: String]] = []
-    private var cancellables = Set<AnyCancellable>()
     private var isInitialized = false
     
-    func initialize() {
-        if !isInitialized {
-            let initialPrompt = "You are a digital assistant that helps people better understand their feelings."
-            sendMessage(message: initialPrompt, shouldTrackResponse: false)
-            isInitialized = true
-        }
+    var conversationHistory: [[String: String]] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    enum Role: String {
+        case user = "user"
+        case assistant = "assistant"
     }
     
-    func sendMessage(message: String, shouldTrackResponse: Bool = true) {
-        let userMessage = ["role": "user", "content": message]
+    
+    func initialize(initialPrompt: String) {
+        guard !isInitialized else { return }
+        
+        sendMessage(message: "\(initialPrompt) Incearca sa nu depasesti 512 tokenuri. Inainte de a genera o lista, scrie '@TITLE@', apoi ofera un titlu listei, inconjurat de trei stelute (*). Sub titlu scrie '@LIST@'. Dupa ce termini de generat itemii listei, scrie, din nou '@LIST@'. Fiecare item sa fie de forma: '1. **Titlu 1** - Corp 1'. ETICHETAREA IN ACEST FEL ESTE ESENTIALA SI TREBUIE FACUTA CHIAR SI PENTRU LISTE CU UN SINGUR ITEM. Nu include intrebari in itemii listei. Genereaza o singura lista pentru fiecare mesaj. Orice sfat / recomandare este generat sub forma unei liste. Daca utilizatorul iti cere itemi suplimentari la o lista deja existenta, foloseste ACELASI titlu (NU SCHIMBA NICI MACAR O LITERA DIN TITLU) si incepe cu urmatorul index (ex: daca lista deja existenta are 7 itemi, primul articol din lista noua cu acelasi subiect va avea indexul 8). Ai voie sa generezi liste doar cand dai sfaturi sau lucruri care ar trebui sa fie memorate de catre utilizator. Aceste reguli sunt necesare pentru functionarea programului. Nu ai voie sa te abati de la ele. Abaterea este strict interzisa. Abaterea inseamna moarte. te pup", shouldTrackResponse: true)
+        isInitialized = true
+    }
+    
+    func sendMessage(message: String, shouldTrackResponse: Bool = true, retryCount: Int? = nil, isSummary: Bool = false) {
+        let retryCount = retryCount ?? defaultRetryCount
+        let userMessage = ["role": Role.user.rawValue, "content": message]
         conversationHistory.append(userMessage)
         
-        let body = OpenAICompletionsBody(model: "gpt-3.5-turbo", messages: conversationHistory, temperature: 0.5, max_tokens: 256)
         
-        let headers: HTTPHeaders = [
-            "Authorization": "Bearer \(openAIKey)"
-        ]
+        let body = createRequestBody()
         
-        isLoading = true
+        let headers: HTTPHeaders = ["Authorization": "Bearer \(Keys().openAIKey)"]
+        
+        if isSummary {
+            isSummaryLoading = true
+        } else {
+            isLoading = true
+        }
         errorMessage = nil
+        
         AF.request(baseURL, method: .post, parameters: body, encoder: .json, headers: headers)
             .validate()
             .publishDecodable(type: OpenAIResponse.self)
-            .timeout(.seconds(10), scheduler: DispatchQueue.main)
+            .timeout(.seconds(20), scheduler: DispatchQueue.main)
+            .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { completion in
-                self.isLoading = false
-                self.isInitialLoading = false
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("Error: \(error)")
-                    self.errorMessage = "Error: \(error.localizedDescription)"
-                }
+                self.handleCompletion(completion, message: message, shouldTrackResponse: shouldTrackResponse, retryCount: retryCount, isSummary: isSummary)
             }, receiveValue: { response in
-                self.isLoading = false
-                self.isInitialLoading = false
-                if let value = response.value, let content = value.choices.first?.message.content {
-                    if shouldTrackResponse {
-                        self.responseMessage = content
-                    }
-                    let assistantMessage = ["role": "assistant", "content": content]
-                    self.conversationHistory.append(assistantMessage)
-                } else if let error = response.error {
-                    self.errorMessage = "Error: \(error.localizedDescription)"
-                }
+                self.handleResponse(response, shouldTrackResponse: shouldTrackResponse, isSummary: isSummary)
             })
             .store(in: &self.cancellables)
     }
+    
+    func summarizeConversation() {
+        let summaryPrompt = "SISTEM: Genereaza un rezumat scurt al conversatiei, ce surprinde infomatiile esentiale. Este scris la persoana intai, sub forma unei pagini de jurnal (monolog, nu voberste cu jurnalul). Limbajul sa NU fie robotic, ci fluid, uman si firesc. NU inventa nimic. Daca s-a discutat nimic, spui asta. Limita de tokenuri este, acum, 2000."
+        sendMessage(message: summaryPrompt, shouldTrackResponse: true, isSummary: true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private func createRequestBody() -> OpenAICompletionsBody {
+        return OpenAICompletionsBody(model: model, messages: conversationHistory, temperature: temperature, max_tokens: maxTokens)
+//        return OpenAICompletionsBody(model: "gpt-3.5-turbo", messages: conversationHistory, temperature: 0.5, max_tokens: 256)
+    }
+    
+    private func handleCompletion(_ completion: Subscribers.Completion<DataResponsePublisher<OpenAIResponse>.Failure>, message: String, shouldTrackResponse: Bool, retryCount: Int, isSummary: Bool) {
+        if isSummary {
+            self.isSummaryLoading = false
+        } else {
+            self.isLoading = false
+        }
+        self.isInitialLoading = false
+        
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print("Request failed with error: \(error)")
+            if retryCount > 0 {
+                print("Retrying... \(retryCount) attempts left.")
+                self.sendMessage(message: message, shouldTrackResponse: shouldTrackResponse, retryCount: retryCount - 1, isSummary: isSummary)
+            } else {
+                if !self.isInitialized {
+                    self.responseMessage = notWorkingErrorResponseMessage
+                }
+                self.errorMessage = "Error: \(error.localizedDescription)"
+            }
+        }
+    }
+    
+    private func handleResponse(_ response: DataResponse<OpenAIResponse, AFError>, shouldTrackResponse: Bool, isSummary: Bool) {
+        if isSummary {
+            self.isSummaryLoading = false
+        } else {
+            self.isLoading = false
+        }
+        self.isInitialLoading = false
+        
+        print("Received response: \(response)")
+        
+        if let value = response.value, let content = value.choices.first?.message.content {
+            let trimmedContent = content.trimmingCharacters(in: .whitespacesAndNewlines)
+            if shouldTrackResponse {
+                if isSummary {
+                    self.summaryMessage = trimmedContent
+                } else {
+                    self.responseMessage = trimmedContent
+                    let assistantMessage = ["role": Role.assistant.rawValue, "content": trimmedContent]
+                    self.conversationHistory.append(assistantMessage)
+                }
+            }
+        } else if let error = response.error {
+            print("Response contains error: \(error)")
+            if !self.isInitialized {
+                self.responseMessage = notWorkingErrorResponseMessage
+            }
+            self.errorMessage = "Error: \(error.localizedDescription)"
+        }
+    }
 }
-
-
-//class OpenAIService: ObservableObject {
-//    let baseURL = "https://api.openai.com/v1/chat/completions"
-//    @Published var responseMessage: String = ""
-//    var conversationHistory: [[String: String]] = []
-//    
-//    func sendMessage(message: String) {
-//        let userMessage = ["role": "user", "content": message]
-//        conversationHistory.append(userMessage)
-//        
-//        let body = OpenAICompletionsBody(model: "gpt-3.5-turbo", messages: conversationHistory, temperature: 0.5, max_tokens: 256)
-//        
-//        let headers: HTTPHeaders = [
-//            "Authorization": "Bearer \(openAIKey)"
-//        ]
-//        AF.request(baseURL, method: .post, parameters: body, encoder: .json, headers: headers).responseDecodable(of: OpenAIResponse.self) { response in
-//            switch response.result {
-//            case .success(let value):
-//                if let content = value.choices.first?.message.content {
-//                    self.responseMessage = content
-//                    let assistantMessage = ["role": "assistant", "content": content]
-//                    self.conversationHistory.append(assistantMessage)
-//                }
-//            case .failure(let error):
-//                print("Error: \(error)")
-//            }
-//        }
-//    }
-//}
 
 
 
